90980e2fb37b2e7910e9678f9524eeb5f3684e8a
Gaurav Kumar <log4gaurav@gmail.com>
2022-11-11 11:23:59 +0530

    added this file

---
+++ component/footer.js
@@ -0,0 +1,52 @@
+function footer(){
+    return ` <div id="left">
+    <div id="trust">
+        <img src="https://www.pngitem.com/pimgs/m/93-932042_5-star-trustpilot-flag-hd-png-download.png">
+    </div>
+    <div id="icon">
+        <img src="https://p.kindpng.com/picc/s/10-103587_facebook-twitter-instagram-clipart-icon-instagram-twitter-and.png">
+    </div>
+    <div id="store">
+    <a href="https://apps.apple.com/gb/app/lyst-shop-fashion-brands/id597940518"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbTuHL8EnMZJX15u7RhzVQQI4zfnO76FnDgA&usqp=CAU" alt=""></a>
+    <a href="https://play.google.com/store/apps/details?id=com.lyst.lystapp"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1hEAX0YIN7n3awAMXbAi5vuFnj_K7mb7APA&usqp=CAU" alt=""></a>
+    </div>
+    <p>Learn about the Lyst app for iPhone, iPad and Android.</p>
+    <p>© 2022 Lyst</p>
+</div>
+<div id="INTERNATIONAL">
+    <h3>INTERNATIONAL</h3>
+    <p>Lyst - AU</p>
+    <p>Lyst - CA</p>
+    <p>Lyst - UK</p>
+    <p>Lyst - Österreich</p>
+    <p>Lyst - Schweiz</p>
+    <p>Lyst - Deutschland</p>
+    <p>Lyst - España</p>
+    <p>Lyst - France</p>
+    <p>Lyst - Italia</p>
+    <p>Lyst - 日本</p>
+    <p>Lyst - België</p>
+    <p>Lyst - Nederland</p>
+</div>
+<div id="HELP">
+    <h3>HELP AND INFO</h3>
+    <p>Help center</p>
+    <p> About us</p>
+    <p>Shipping policy</p>
+    <p>Returns policy</p>
+    <p>Payments</p>
+    <p>Refund policy</p>
+    <p>Developers</p>
+    <p>Careers</p>
+    <p>Contact</p>
+    <p>Terms & conditions</p>
+    <p>Privacy & cookie policy</p>
+    <p>Privacy & cookie policy</p>
+    <p>Intellectual property</p>
+    <p>Categories</p>
+    <p>Become a partner</p>
+    <p>Lyst Insights</p>
+    <p>Lyst News</p>
+</div>`
+}
+export{footer}
---
+++ footer.js
@@ -0,0 +1,3 @@
+import { footer } from "./component/footer.js";
+let Footer=document.getElementById("footer");
+Footer.innerHTML=footer()
